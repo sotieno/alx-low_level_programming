a02bde0081da8871468b0b90cd3806e3124f1036
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-09-09 21:26:19 +0000

    Print alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,20 @@
 
 int main(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i, j;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alphabet[i]);
+		putchar(lower[i]);
 	}
+
+	for (j = 0; j < 26; j++)
+	{
+		putchar(upper[j]);
+	}
+
 	putchar('\n');
 return (0);
 
