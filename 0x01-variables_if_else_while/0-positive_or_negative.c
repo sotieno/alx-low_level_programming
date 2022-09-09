06fe566d79c62b69a1dd4abf8fd4c2b172299682
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-09-09 13:26:23 +0000

    Use of IF...ELSE and ELSE...IF statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* IF...ELSE and ELSE...IF statement */
-	if( n > 0 )
+	if(n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if ( n == 0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
