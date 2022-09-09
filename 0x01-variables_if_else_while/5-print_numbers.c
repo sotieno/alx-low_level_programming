4be1d0de9255d7c7acf21ebc5def52991acded97
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-09-09 21:42:50 +0000

    Print single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase\n
+ * Return: 0 when main function executes successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+return (0);
+
+}
