54f2cb9dbe98241b42f6c46576df9cdbbf9cd5ea
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-09-14 12:19:38 +0000

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar followed by a new line.
+ * Return: 0 if main executes successfully
+ */
+
+int main(void)
+{
+    char str[8] = "_putchar";
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+    return (0);
+
+}
