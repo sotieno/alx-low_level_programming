5a10bb6501b9fb69c8d31094cedec4ab17c4d22b
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2022-09-15 12:50:54 +0000

    Return largest of 3 numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,13 +20,9 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if ((c > a && a > b) || (c > b && b > a))
-	{
-		largest = c;
-	}
 	else
 	{
-		printf("We have a problem!");
+		largest = c;
 	}
 
 	return (largest);
